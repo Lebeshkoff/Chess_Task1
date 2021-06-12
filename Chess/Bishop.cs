47177ef737c9a1ba8d6c41dd8e9aaddc66ad668b
Dmitry Lebeshkov <51168295+Lebeshkoff@users.noreply.github.com>
2021-06-12 23:30:26 +0300

    Add logic to Bishop

--- Chess/Bishop.cs
+++ Chess/Bishop.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Chess
 {
@@ -6,16 +8,51 @@ namespace Chess
     {
         public Bishop(Point2D position)
         {
-            this.Position = position;
+            Position = position;
         }
         public override IEnumerable<Point2D> GetValidMovements(Board board)
         {
-            throw new System.NotImplementedException();
+            var valMoves = new List<Point2D>();
+            valMoves.AddRange(GetValidPositions(new Point2D(1,1), board));
+            valMoves.AddRange(GetValidPositions(new Point2D(-1,1), board));
+            valMoves.AddRange(GetValidPositions(new Point2D(-1,-1), board));
+            valMoves.AddRange(GetValidPositions(new Point2D(1,-1), board));
+            return valMoves;
+        }
+
+        private IEnumerable<Point2D> GetValidPositions(Point2D axis, Board board)
+        {
+            var resList = new List<Point2D>();
+            var tempPosition = Position; 
+            while (tempPosition.X < 8 && tempPosition.X >= 0 && tempPosition.Y < 8 && tempPosition.Y >= 0)
+            {
+                tempPosition += axis;
+                if (board.BlackPlayer.figures.Any(figure => figure.Position == tempPosition))
+                {
+                    return resList;
+                }
+                if (board.WhitePlayer.figures.Any(figure => figure.Position == tempPosition))
+                {
+                    return resList;
+                }
+                resList.Add(tempPosition);
+            }
+
+            return resList;
         }
 
         public override void Move(Point2D position, Board board)
         {
-            throw new System.NotImplementedException();
+            foreach (var valPosition in GetValidMovements(board))
+            {
+                if (position == valPosition)
+                {
+                    Position = valPosition;
+                    return;
+                }
+            }
+
+            throw new Exception("No valid move!");
         }
     }
 } 
