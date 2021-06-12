6715e5992af6ae2afdeaec842ed7c3df0c757cc5
Dmitry Lebeshkov <51168295+Lebeshkoff@users.noreply.github.com>
2021-06-13 00:12:02 +0300

    Add logic to Rook

--- Chess/Rook.cs
+++ Chess/Rook.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Chess
 {
@@ -6,16 +8,55 @@ namespace Chess
     {
         public Rook(Point2D position)
         {
-            base.Position = position;
+            Position = position;
         }
         public override IEnumerable<Point2D> GetValidMovements(Board board)
         {
-            throw new System.NotImplementedException();
+            var valMoves = new List<Point2D>();
+            //Up
+            valMoves.AddRange(GetValidPositions(new Point2D(0,1), board));
+            //Down
+            valMoves.AddRange(GetValidPositions(new Point2D(0,-1), board));
+            //Left
+            valMoves.AddRange(GetValidPositions(new Point2D(-1,0), board));
+            //Right
+            valMoves.AddRange(GetValidPositions(new Point2D(1,0), board));
+            return valMoves;
         }
-
-        public override void Move(Point2D position, Board board)
+        
+        private IEnumerable<Point2D> GetValidPositions(Point2D axis, Board board)
         {
-            throw new System.NotImplementedException();
+            var resList = new List<Point2D>();
+            var tempPosition = Position; 
+            while (tempPosition.X < 8 && tempPosition.X >= 0 && tempPosition.Y < 8 && tempPosition.Y >= 0)
+            {
+                tempPosition += axis;
+                if (board.BlackPlayer.figures.Any(figure => figure.Position == tempPosition))
+                {
+                    return resList;
+                }
+                if (board.WhitePlayer.figures.Any(figure => figure.Position == tempPosition))
+                {
+                    return resList;
+                }
+                resList.Add(tempPosition);
+            }
+
+            return resList;
+        }
+        
+        public override void Move(Point2D position, Board board, Color color)
+        {
+            foreach (var valPosition in GetValidMovements(board))
+            {
+                if (position == valPosition)
+                {
+                    Position = valPosition;
+                    return;
+                }
+            }
+
+            throw new Exception("No valid move!");
         }
     }
 }
