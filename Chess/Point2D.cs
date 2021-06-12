02ab7e3bcab5bea7e8fdeb7c8470cb5157e0a82a
Dmitry Lebeshkov <51168295+Lebeshkoff@users.noreply.github.com>
2021-06-12 23:29:24 +0300

    Add overload operator + in Point2D

--- Chess/Point2D.cs
+++ Chess/Point2D.cs
@@ -24,5 +24,10 @@ namespace Chess
             X = x;
             Y = y;
         }
+
+        public static Point2D operator +(Point2D point1, Point2D point2)
+        {
+            return new Point2D(point1.X + point2.X, point1.Y + point2.X);
+        }
     }
 }
