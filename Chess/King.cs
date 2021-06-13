f02a44aa47fd81398e0c641b5d4b5f4fc968163e
Dmitry Lebeshkov <51168295+Lebeshkoff@users.noreply.github.com>
2021-06-13 19:42:36 +0300

    Fix remove no valid points

--- Chess/King.cs
+++ Chess/King.cs
@@ -23,11 +23,11 @@ namespace Chess
             valMoves.Add(new Point2D(Position.X - 1, Position.Y - 1));
             valMoves.Add(new Point2D(Position.X, Position.Y - 1));
             valMoves.Add(new Point2D(Position.X + 1, Position.Y - 1));
-            
-            foreach (var valMove in valMoves.Where(valMove => valMove.X > 7 || valMove.Y > 7 || valMove.X < 0 || valMove.Y < 0))
-            {
-                valMoves.Remove(valMove);
-            }
+
+            var removeCollection = valMoves.Where(valMove => valMove.X > 7 || 
+                                                             valMove.Y > 7 || 
+                                                             valMove.X < 0 || 
+                                                             valMove.Y < 0).ToList();
 
             foreach (var figure in Color == Color.White? board.WhitePlayer.figures : board.BlackPlayer.figures) 
             { 
@@ -35,11 +35,15 @@ namespace Chess
                 {
                     if (figure.Position == valMove)
                     {
-                        valMoves.Remove(valMove);
+                        removeCollection.Add(valMove);
                     }
                 }
             }
 
+            foreach (var toDelete in removeCollection)
+            {
+                valMoves.Remove(toDelete);
+            }
             return valMoves;
         }
     }
