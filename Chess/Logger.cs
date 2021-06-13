ceaf696f69ee5c273848656d79732872989f457e
Dmitry Lebeshkov <51168295+Lebeshkoff@users.noreply.github.com>
2021-06-13 18:00:33 +0300

    Add static class who log game

--- Chess/Logger.cs
+++ Chess/Logger.cs
@@ -1,7 +1,20 @@
+using System;
+using System.Collections.Generic;
+
 namespace Chess
 {
-    public class Logger
+    public static class Logger
     {
-        
+        private static List<string> _logList = new List<string>();
+
+        public static void AddActionToLog(string log)
+        {
+            _logList.Add(log);
+        }
+
+        public static List<string> GetLogList()
+        {
+            return _logList;
+        }
     }
 }
