8ab2af59101eee8c0ef9a689bc920cdc0bfa58d1
Dmitry Lebeshkov <51168295+Lebeshkoff@users.noreply.github.com>
2021-06-13 14:58:30 +0300

    Minor update in Player Add logger class

--- Chess/Player.cs
+++ Chess/Player.cs
@@ -7,30 +7,31 @@ namespace Chess
     public class Player
     {
         public List<Figure> figures = new List<Figure>();
-        private readonly Color _color;
+
+        public readonly Color color;
 
         public Player(Color color)
         {
-            _color = color;
+            this.color = color;
             InitFigures();
         }
 
         private void InitFigures()
         {
-            if (_color == Color.White)
+            if (color == Color.White)
             {
                 for (int i = 0; i < 8; i++)
                 {
                     figures.Add(new Pawn(new Point2D(i, 1), Color.White));
                 }
-                figures.Add(new Rook(new Point2D(0,0), _color));
-                figures.Add(new Rook(new Point2D(7,0), _color));
-                figures.Add(new Knight(new Point2D(1,0), _color));
-                figures.Add(new Knight(new Point2D(6,0), _color));
-                figures.Add(new Bishop(new Point2D(2, 0), _color));
-                figures.Add(new Bishop(new Point2D(5, 0), _color));
-                figures.Add(new Queen(new Point2D(3, 0), _color));
-                figures.Add(new King(new Point2D(4, 0), _color));
+                figures.Add(new Rook(new Point2D(0,0), color));
+                figures.Add(new Rook(new Point2D(7,0), color));
+                figures.Add(new Knight(new Point2D(1,0), color));
+                figures.Add(new Knight(new Point2D(6,0), color));
+                figures.Add(new Bishop(new Point2D(2, 0), color));
+                figures.Add(new Bishop(new Point2D(5, 0), color));
+                figures.Add(new Queen(new Point2D(3, 0), color));
+                figures.Add(new King(new Point2D(4, 0), color));
             }
             else
             {
@@ -38,14 +39,14 @@ namespace Chess
                 {
                     figures.Add(new Pawn(new Point2D(i, 6), Color.Black));
                 }
-                figures.Add(new Rook(new Point2D(0,7), _color));
-                figures.Add(new Rook(new Point2D(7,7), _color));
-                figures.Add(new Knight(new Point2D(1,7), _color));
-                figures.Add(new Knight(new Point2D(6,7), _color));
-                figures.Add(new Bishop(new Point2D(2, 7), _color));
-                figures.Add(new Bishop(new Point2D(5, 7), _color));
-                figures.Add(new Queen(new Point2D(3, 7), _color));
-                figures.Add(new King(new Point2D(4, 7), _color));
+                figures.Add(new Rook(new Point2D(0,7), color));
+                figures.Add(new Rook(new Point2D(7,7), color));
+                figures.Add(new Knight(new Point2D(1,7), color));
+                figures.Add(new Knight(new Point2D(6,7), color));
+                figures.Add(new Bishop(new Point2D(2, 7), color));
+                figures.Add(new Bishop(new Point2D(5, 7), color));
+                figures.Add(new Queen(new Point2D(3, 7), color));
+                figures.Add(new King(new Point2D(4, 7), color));
             }
         }
 
